42e975a983f35b6a72efbfa5bbacd637a055b06d
Jin Su Park <jinsupark@icloud.com>
2023-01-27 04:39:32 +1300

    Add

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,22 +2,64 @@
 document.addEventListener('alpine:init', () => {
   // Main
   Alpine.data('main', () => ({
+
     iconSettings: {
-      roundness: 3,
+      cornerRadiusSliderValue: 2,
+      cornerRadius: 4,
       strokeWidthSliderValue: 10,
-      strokeWidth: 2
+      strokeWidth: 2,
+      styles: {
+        line: true,
+        duoLine: false,
+        duoTone: false,
+        duoFill: false,
+        solid: false
+      }
+    },
+
+    explorerSettings: {
+      gridSize: 'is-md'
+    },
+
+    // Icon Search Component
+    icons: [],
+    searchTerm: '',
+    filteredIcons: [],
+
+    init() {
+
+    },
+    filterIcons() {
+      console.log(this.searchTerm);
+      let searchTerm = this.searchTerm.toLowerCase();
+      let icons = document.querySelectorAll('.grid-icon');
+      console.log(icons.length);
+      for (let i = 0; i < icons.length; i++) {
+        
+        let searchContent = icons[i].getAttribute('data-keywords').toLowerCase();
+        console.log(searchContent);
+        icons[i].style.display = searchContent.includes(searchTerm)
+          ? 'flex'
+          : 'none';
+        console.log('hello');
+      }
     },
     roundTo(n, place) {
       return +(Math.round(n + 'e+' + place) + 'e-' + place);
     },
     changeStrokeWidth(event) {
-
-      this.iconSettings.strokeWidth = this.roundTo((this.iconSettings.strokeWidthSliderValue * 0.1) + 1, 2);
+      this.iconSettings.strokeWidth = this.roundTo(
+        Number(this.iconSettings.strokeWidthSliderValue) * 0.1 + 1,
+        2
+      );
       document.querySelectorAll('.grid-icon path').forEach((el, i) => {
         el.setAttribute('stroke-width', this.iconSettings.strokeWidth);
       });
-      console.log(this.iconSettings.strokeWidth);
-    }
+    },
+    changeCornerRadius(event) {
+      this.iconSettings.cornerRadius =
+        Number(this.iconSettings.cornerRadiusSliderValue) * 2;
+    },
   }));
 });
 
